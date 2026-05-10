0c927a8c6926c4dec6b772aa5124d166afb9018c
doryoysterpie <ilbricconemarketing@gmail.com>
2026-05-10 18:31:07 -0400

    Delete Solana (1) 2 directory

--- Solana (1) 2/src/lib/configAddress.ts
+++
@@ -1,3 +0,0 @@
-// Config address is null until initialize_config is called by the authority.
-// For now, the program does not require a global config for user-facing instructions.
-export const configAddress = "";
--- Solana (1) 2/src/polyfills.ts
+++
@@ -1,9 +0,0 @@
-import { Buffer } from "buffer";
-
-// Polyfill Buffer for Solana wallet adapters
-if (typeof window !== "undefined") {
-  if (!window.Buffer) {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    (window as any).Buffer = Buffer;
-  }
-}
